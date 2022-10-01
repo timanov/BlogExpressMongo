4fe0b754cd2e0555f914778ea8eada63f192ea49
Mikhail Timanov <timanov.testing@gmail.com>
2022-10-02 00:00:02 +0300

    Научили express читать json

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import exress from 'express';
 
 const app = exress();
 
+app.use(exress.json());
+
 app.get('/', (req, res) => {
     res.send('1  Hello World');
 });
 
 app.post('/auth/login', (req, res) => {
+    console.log(req.body);
     res.json({
         success: true,
     });
