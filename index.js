aeebe4d630333c583012da5bbe7a1be4050c4757
Mikhail Timanov <timanov.testing@gmail.com>
2022-10-01 23:29:49 +0300

    Обработали GET запрос, теперь возвращается в ответе hello world, приложение запустили

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import exress from 'express';
+
+const app = exress();
+
+app.
+get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+})
